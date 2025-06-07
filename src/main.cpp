58fe8563ebc97a78b4c7fea21baa127ff37b54b4
Luiz Felipe Pinto Avila de Barros <lfpab@windows.cin.ufpe.br>
2025-06-07 10:16:48 -0300

    GuitarCIn

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <LiquidCrystal_I2C.h>
 
 // Criando o objeto lcd que é o visor:
-LiquidCrystal_I2C lcd(0x20, 16, 2); // Endereço e tamanho do LCD
+LiquidCrystal_I2C lcd(0x27, 16, 2); // Endereço e tamanho do LCD
 
 #define red 3
 #define blue 4
@@ -11,7 +11,7 @@ LiquidCrystal_I2C lcd(0x20, 16, 2); // Endereço e tamanho do LCD
 #define yellow 6
 #define buzzer 7
 #define button 8
-#define do 262
+#define do_note 262
 #define re 294
 #define mi 330
 #define la 440
@@ -21,33 +21,23 @@ bool game_over = false;
 int pontuacao = 0;
 int recorde = 0;
 
-void run_GuitarCIn(float &game_time, bool &game_over){
+void run_GuitarCIn(float &game_time, bool &game_over, int &pontuacao){
   int num = random(3, 7); // Gera um número aleatório, cada qual atrelado a uma nota: 3 - vermelho, 4 - azul, 5 - verde, 6 - amarelo
-
-  // Exibindo os movimentos da notas(números) no monitor serial
-  for(int i = 0;i < 4; i++){
-    if(i == num - 3){
-      Serial.print("1");
-    }
-    else{
-      Serial.print("0");
-    }
-    Serial.print("  ");
-  }
-  Serial.println("");
 
   // Printando as notas no visor: -----------------------------------------------------------------------
   lcd.clear();
   for(int i = 0; i < 4; i++){
-    if(i == num - 3){
-      lcd.setCursor(i, 0);
-      lcd.write(byte(1)); // Nota correta
-      lcd.write(' ');
+    if(i == num - 3){ // Nota certa
+      lcd.setCursor(3 * i + 1, 0);
+      lcd.print("()");
+      lcd.setCursor(3 * i + 1, 1);
+      lcd.print("()");
     }
     else{
-      lcd.setCursor(i, 0);
-      lcd.write(byte(0)); // Nota errada
-      lcd.write(' ');
+      lcd.setCursor(3 * i + 1, 0);
+      lcd.print("||");
+      lcd.setCursor(3 * i + 1, 1);
+      lcd.print("||");
     }
   }
   delay(game_time);
@@ -86,16 +76,20 @@ void run_GuitarCIn(float &game_time, bool &game_over){
   else{
     // Caso a resposta do jogador tenha sido acertiva, iremos tocar a nota correspondente:
     if(reaction == 3){
-      tone(buzzer, do, 100);
+      tone(buzzer, do_note, 100);
+      delay(50);
     }
     else if(reaction == 4){
       tone(buzzer, re, 100);
+      delay(50);
     }
     else if(reaction == 5){
       tone(buzzer, mi, 100);
+      delay(50);
     }
     else if(reaction == 6){
       tone(buzzer, la, 100);
+      delay(50);
     }
     pontuacao += 1;
   }
@@ -103,7 +97,7 @@ void run_GuitarCIn(float &game_time, bool &game_over){
   if(game_time > 900){
     game_time -= 80;
   }
-  else if(900 <= game_time < 600){ // O jogo demora mais para acelerar quando chega nesse estágio, para evitar que chegue na velocidade máxima rápido demais.
+  else if(game_time <= 900 && game_time > 600){ // O jogo demora mais para acelerar quando chega nesse estágio, para evitar que chegue na velocidade máxima rápido demais.
     game_time -= 40;
   }
 }
@@ -126,40 +120,55 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  run_GuitarCIn(game_time, game_over);
+  run_GuitarCIn(game_time, game_over, pontuacao);
 
   if(game_over){
     lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print("Sua pontuação foi de ");
-    lcd.setCursor(6,0);
+    lcd.print("Sua pontuacao");
+    lcd.setCursor(0, 1);
+    lcd.print("foi de: ");
+    lcd.setCursor(8,1);
     lcd.print(pontuacao);
-    Serial.print("Sua pontuação foi de: ");
-    Serial.println(pontuacao);
+    delay(1800);
 
     // Checando se o recorde foi batido:
     if(pontuacao > recorde){
       lcd.clear();
       lcd.setCursor(0,0);
-      lcd.print("Parabéns!! Você acabou de bater o recorde que era de ");
-      lcd.setCursor(8,0);
+      lcd.print("Parabens!! Voce");
+      lcd.setCursor(0,1);
+      lcd.print("bateu o recorde!");
+      delay(1800);
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("O recorde");
+      lcd.setCursor(0, 1);
+      lcd.print("era de: ");
+      lcd.setCursor(8, 1);
       lcd.print(recorde);
-      Serial.print("Parabéns!! Você acabou de bater o recorde que era de ");
-      Serial.println(recorde);
+      delay(2100);
       recorde = pontuacao;
     }
 
     // Recomeçando o jogo:
-    pontuacao = 0;
-    Serial.println("Bem vindo ao GuitarCIn! Pressione o botão para começar");
     lcd.clear();
     lcd.setCursor(0,0);
-    lcd.print("Bem vindo ao GuitarCIn! Pressione o botão para começar");
+    lcd.print("Bem vindo");
+    lcd.setCursor(0, 1);
+    lcd.print("ao GuitarCIn!");
+    delay(1500);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Para dar start,");
+    lcd.setCursor(0,1);
+    lcd.print("aperte o botao");
     int decisao = digitalRead(button);
     while(decisao != LOW){
         decisao = digitalRead(button); // Esperando o jogador iniciar o jogo
         delay(100); //debounce
     }
+    pontuacao = 0;
     game_over = false; // O jogo foi recomeçado
     game_time = 2000;
   }
